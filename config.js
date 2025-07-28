cd9fdc9b1a10ee28c12f5e0761b15509daa872a9
rashmikahirusan2008 <rashmikahirusan4@gmail.com>
2025-07-28 10:11:37 +0530

    Update config.js

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "4chmDBqQ#qhBuRGXS2BzUa3DImUyZFkK4JpyrlhcPKyETO3quEOM",
+SESSION_ID: process.env.SESSION_ID || "tUQBSRZR#WCM_8zxmLhecZ-X7HnGYPIy5Xm3JfiGWVRhW3X9QqAU",
 ALIVE_IMG: process.env.ALIVE_IMG || "https://raw.githubusercontent.com/rashmikahirusan2008/Rash-md-/refs/heads/main/imajas/IMG-20250727-WA0017.jpg",
 ALIVE_MSG: process.env.ALIVE_MSG || "*Hello👋 DANUWA-MD Is Alive Now😍*",
 BOT_OWNER: '94776121326',  // Replace with the owner's phone number
